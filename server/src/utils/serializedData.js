bc3735ef36ddafde8fafdbd65ccc0de192c9d366
leandro-wrf <leandro.ferreira.wr@gmail.com>
2020-10-04 15:09:07 -0300

    alterei a forma que os dados chegam

--- server/src/utils/serializedData.js
+++ server/src/utils/serializedData.js
@@ -2,7 +2,7 @@ module.exports = (data) => {
   return data.map((contact) => {
     return {
       ...contact.dataValues,
-      image: `http://192.178.31.105:3333/uploads/${contact.dataValues.image}`,
+      image: `http://localhost:3333/uploads/${contact.dataValues.image}`,
     };
   });
 };
